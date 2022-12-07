44def9b7c5c6e01541b9c614bc0d5316a35a6d7d
Sahib <48856153+sahibcode@users.noreply.github.com>
2022-12-07 23:33:02 +0500

    Add files via upload

---
+++ twitter_scrape.py
@@ -0,0 +1,31 @@
+import snscrape.modules.twitter as sntwitter
+import pandas as pd
+
+
+
+
+# your query goes here, account to scrape and since when to scrape
+query = "(from:MadNews_io) until:2022-12-01 since:2022-01-01"
+
+# creating list to append tweet data to
+tweets = []
+
+# maximum number of tweets to scrape
+limit = 10000
+
+# using TwitterSearchScraper to scrape
+for tweet in sntwitter.TwitterSearchScraper(query).get_items():
+
+    if len(tweets) == limit:
+        break
+    else:
+        tweets.append([tweet.date, tweet.username, tweet.content])
+
+
+# creating a dataframe from the tweets list above
+df = pd.DataFrame(tweets, columns=['Date', 'User', 'Tweet'])
+
+
+# to save to csv
+df.to_csv('tweets.csv')
+
